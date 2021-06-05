b2f91b55bb3c76384442fc75815d339f4f9d54b0
michael <michael@n.c>
2021-06-05 16:39:58 +0000

    added stats

--- stats.sh
+++ stats.sh
@@ -1,6 +1,6 @@
-find reports/*c1* | while read f ; do 
+find reports/*.txt | sort |while read f ; do 
 	echo -n $f; tail -20 $f | egrep -i \
 		"(fail|BUILD SUCCESSFUL in|^.INFO. BUILD SUCCESS$|ELAPSED TIME)"|\
-		tr "\n" " "| sed -e "s/BUILD SUCCESS.*ELAPSED TIME/SUCCESS/"
+		tr "\n" " "| sed -e "s/BUILD SUCCESS.*ELAPSED TIME/ SUCCESS/;s/\[INFO\]//"|tr -s " "
 	echo
 done
