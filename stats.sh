b04778aa3123293dc6fb8d4df6cd5a9228d17f0a
michael <gitlab@net128.com>
2021-06-05 19:15:05 +0200

    stats...

--- stats.sh
+++ stats.sh
@@ -1,6 +1,11 @@
 find reports/*.txt | sort |while read f ; do 
-	echo -n $f; tail -20 $f | egrep -i \
+	printf "%s: " "$f"
+	tail -20 $f | egrep -i \
 		"(fail|BUILD SUCCESSFUL in|^.INFO. BUILD SUCCESS$|ELAPSED TIME)"|\
-		tr "\n" " "| sed -e "s/BUILD SUCCESS.*ELAPSED TIME/ SUCCESS/;s/\[INFO\]//"|tr -s " "
+		tr "\n" " "| sed -e "s/BUILD SUCCESS.*ELAPSED TIME/ SUCCESS/;s/\[INFO\]//
+		s/BUILD FAILURE.*ELAPSED TIME/FAILURE/;
+		s/: .*BUILD FAILED in.*ELAPSED TIME/FAILURE/;
+		s/[A-Z0-9]*FAILURE/FAILURE/;
+		"|tr -s " "
 	echo
 done
