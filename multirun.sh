def5f68845f358b80d46ea03b5c2fd5485ad6b5f
mike-seger <github@net128.com>
2021-11-22 09:42:11 +0100

    Update multirun.sh

--- multirun.sh
+++ multirun.sh
@@ -1,10 +1,10 @@
 cd $(dirname "$0")
 
-sh ./run.sh dl
-sh ./run.sh
-sh ./run.sh
-sh ./run.sh
-sh ./run.sh
-sh ./run.sh
+sh run.sh dl
+sh run.sh
+sh run.sh
+sh run.sh
+sh run.sh
+sh run.sh
 
 sh stats.sh >reports/stats.txt
