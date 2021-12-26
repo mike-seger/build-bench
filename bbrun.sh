5623abda7660c4094817d03d9782fd1f5bc7f672
mike <mike@1>
2021-12-26 18:41:51 +0100

    added bbrun

--- bbrun.sh
+++ bbrun.sh
@@ -1,9 +1,23 @@
+#!/bin/bash
+
+cleanup() {
+    # kill all processes whose parent is this process
+    pkill -P $$
+}
+
+for sig in INT QUIT HUP TERM; do
+  trap "
+    cleanup
+    trap - $sig EXIT
+    kill -s $sig "'"$$"' "$sig"
+done
+trap cleanup EXIT
 
 cd $(dirname "$0")
 ./multirun.sh >/tmp/1 &
 pid=$!
 while [ 1==1 ] ; do
-	ps aux | grep multirun.sh | egrep -v grep >/dev/null
+	ps aux | grep multirun.sh | egrep -v grep >/dev/null || break
 	./watch-stats.sh
 	sleep 10 
 done
--- stopall.sh
+++
@@ -1,4 +0,0 @@
-for in in {1..20} ; do 
-	kill -9 $(ps auxwww | egrep "(java|run.sh)" |
-		grep -v grep |tr -s " "|cut -f2 -d " ")
-done
