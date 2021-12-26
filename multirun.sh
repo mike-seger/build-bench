2cc37a423e6619b7da9e428a020b5abcc3869adb
michael <michael@i7-9700f.lan>
2021-12-26 02:32:35 +0100

    if available use ramdisk as home during build

--- multirun.sh
+++ multirun.sh
@@ -5,14 +5,18 @@ set -e
 parallel=$1
 cd $(dirname "$0")
 
+if [ -d /mnt/ramdisk ] ; then
+	export HOME=/mnt/ramdisk
+fi
+
 mkdir -p reports
 
-#./run.sh dl $parallel
+./run.sh dl $parallel
 ./run.sh $parallel
 ./run.sh $parallel
 ./run.sh $parallel
 ./run.sh $parallel
 ./run.sh $parallel
-./run.sh $parallel
+#./run.sh $parallel
 
 ./stats.sh -f >reports/stats.txt
