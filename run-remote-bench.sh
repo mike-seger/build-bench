70b7decf460cf7692324c39e809729845919dbdb
michael <michael@n.c>
2021-12-28 15:52:56 +0000

    updated scripts

--- run-remote-bench.sh
+++ run-remote-bench.sh
@@ -12,6 +12,12 @@ reportname=$2
 mkdir $reportname
 rmdir $reportname
 ssh $server rm -fR git/build-bench/reports
-ssh $server git/build-bench/run-bench.sh
+set +e
+ssh $server git/build-bench/run-bench.sh 
+echo "benchmark done"
+set -e
+ssh $server ls -al git/build-bench/reports/stats.txt
 scp -r $server:git/build-bench/reports $reportname
+echo "copied reports, shutting down vm"
 ssh $server sudo shutdown -h now
+echo "all finished"
