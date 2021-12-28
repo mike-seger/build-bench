bee9b3b01f276cc0f7f0c48e4749d7925c9a3cf2
michael <michael@n.c>
2021-12-28 14:31:40 +0000

    run-bench.sh

--- run-bench.sh
+++ run-bench.sh
@@ -5,9 +5,7 @@ set -e
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 export TERM=xterm-256color
-echo $HOME
-ls -al
-source "~/.sdkman/bin/sdkman-init.sh"
+source .sdkman/bin/sdkman-init.sh
 
 cd ~/git/build-bench
 ./multirun.sh >/tmp/1 & 
