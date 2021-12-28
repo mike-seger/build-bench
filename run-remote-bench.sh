bd356622a94e2070413b1d9d4618bdb38980cac9
michael <michael@n.c>
2021-12-28 13:57:21 +0000

    run-remote-bench.sh

---
+++ run-remote-bench.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [ $# != 2 ] ; then
+	echo "Usage: $0 <server> <reportname>"
+	exit 1
+fi
+
+server=$1
+reportname=$2
+mkdir $reportname
+rmdir $reportname
+ssh $server rm -fR git/build-bench/reports
+ssh $server git/build-bench/run-bench.sh
+scp -r $server:git/build-bench/reports $reportname
+ssh $server shutdown -h now
