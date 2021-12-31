f8b10ec3c044dc3f23a95d8deb4c70a747a69044
mike-seger <github@net128.com>
2021-12-31 15:49:03 +0000

    Update run-bench.sh

--- run-bench.sh
+++ run-bench.sh
@@ -5,9 +5,15 @@ set -e
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 export TERM=xterm-256color
+if [[ ! -x "$(which javac 2>/dev/null)" || ! -x "$(which mvn 2>/dev/null)" ]]; then
+  if [ -d "$(cd; pwd)/.sdkman" ] ; then
+    source $(cd; pwd)/.sdkman/bin/sdkman-init.sh
+  else
+    echo "Please install a java jdk 11 and maven 3.8.3"
+    exit 1
+  fi
+fi
 if [ -d /x/ ] ; then # Hiren's PE Boot CD
-  HH=$(cd; pwd)
-  source $HH/.sdkman/bin/sdkman-init.sh
   export HOME=/x
 fi
 
