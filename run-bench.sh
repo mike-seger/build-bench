ac796b44284a602b57d3d068bf98f19e32ef0da3
Michael Seger <michael@michaels-imac-1.lan>
2022-04-16 13:16:01 +0200

    ...

--- run-bench.sh
+++ run-bench.sh
@@ -7,7 +7,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 cd $(dirname $0)
 
 export TERM=xterm-256color
-if [[ ! -x "$(which javac 2>/dev/null)" || ! -x "$(which mvn 2>/dev/null)" ]]; then
+if [[ ! -x "$(which javac 2>/dev/null)" ]]; then
   if [ -d "$(cd; pwd)/.sdkman" ] ; then
     source $(cd; pwd)/.sdkman/bin/sdkman-init.sh
   else
